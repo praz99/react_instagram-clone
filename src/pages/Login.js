91da6ff3e8dc2e9370bebee9d12ab0b63042a22c
Praz <t.prazwal@gmail.com>
2021-05-27 17:11:56 +0200

    Initial setup for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
 function Login() {
-  return <p>I am the login page.</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  // const handleLogin = () => { };
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>NO IDEA YET ABOUT TAILWINDCSS</p>
+    </div>
+  );
 }
 
 export default Login;
