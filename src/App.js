555330b7d7cd2202f705faf7d88f62b941d76ac8
Praz <t.prazwal@gmail.com>
2021-06-01 02:04:29 +0200

    add userContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
 
 const Login = lazy(() => import('./pages/Login'));
 const Signup = lazy(() => import('./pages/Sign-up'));
@@ -8,17 +10,20 @@ const Dashboard = lazy(() => import('./pages/Dashboard'));
 const NotFound = lazy(() => import('./pages/Not-found'));
 
 function App() {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} exact />
-          <Route path={ROUTES.SIGN_UP} component={Signup} exact />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} exact />
+            <Route path={ROUTES.SIGN_UP} component={Signup} exact />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function useAuthListener() {
-  useState[user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
@@ -19,7 +19,7 @@ export default function useAuthListener() {
     });
 
     return () => listener();
-  }, [firebase])
+  }, [firebase]);
 
   return { user };
 }
