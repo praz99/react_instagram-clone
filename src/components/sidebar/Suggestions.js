33238d0dd2b995fd4b0e75002a3311e1de3dd409
Praz <t.prazwal@gmail.com>
2021-06-04 18:33:28 +0200

    adds logic for showing suggested profiles.

--- src/components/sidebar/Suggestions.js
+++ src/components/sidebar/Suggestions.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
+import { getSuggestedProfiles } from '../../services/firebase';
 
-function Suggestions({ userId }) {
+function Suggestions({ userId, following }) {
   const [profiles, setProfiles] = useState(null);
 
-  return !profiles ? <Skeleton count={10} height={150} className="mt-5" /> : <p>Hello</p>;
+  useEffect(() => {
+    async function suggestedProfiles() {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+    }
+
+    if (userId) {
+      suggestedProfiles();
+    }
+
+    console.log('profiles', profiles);
+  }, [userId]);
+
+  // eslint-disable-next-line no-nested-ternary
+  return !profiles ? (
+    <Skeleton count={1} height={150} className="mt-5" />
+  ) : profiles.length > 0 ? (
+    <div className="rounded flex flex-col">
+      <div className="text-sm flex items-center align-items justify-between mb-2">
+        <p className="font-bold text-gray-base">Suggestions for you</p>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default Suggestions;
 
 Suggestions.propTypes = {
-  userId: PropTypes.string
+  userId: PropTypes.string,
+  following: PropTypes.array
 };
