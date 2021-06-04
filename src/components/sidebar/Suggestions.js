e3e2373f2739d0b951ab1d887a49d9c47e40977c
Praz <t.prazwal@gmail.com>
2021-06-04 18:53:23 +0200

    display suggested users' username and avatar in suggestions

--- src/components/sidebar/Suggestions.js
+++ src/components/sidebar/Suggestions.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
 import { getSuggestedProfiles } from '../../services/firebase';
+import SuggestedProfile from './Suggested-profile';
 
 function Suggestions({ userId, following }) {
   const [profiles, setProfiles] = useState(null);
@@ -26,6 +27,17 @@ function Suggestions({ userId, following }) {
     <div className="rounded flex flex-col">
       <div className="text-sm flex items-center align-items justify-between mb-2">
         <p className="font-bold text-gray-base">Suggestions for you</p>
+      </div>
+      <div className="mt-4 grid gap-5">
+        {profiles.map((profile) => (
+          <SuggestedProfile
+            key={profile.docId}
+            userDocId={profile.docId}
+            username={profile.username}
+            profileId={profile.userId}
+            userId={userId}
+          />
+        ))}
       </div>
     </div>
   ) : null;
