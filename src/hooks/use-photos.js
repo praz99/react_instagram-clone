7b8ed4e319cc8632f679f4922d5b3ee57b0e7c04
Praz <t.prazwal@gmail.com>
2021-06-07 23:37:43 +0200

    Add function to get the photos of followed users

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,11 +1,26 @@
-import { useContext, useState } from 'react';
-import userContext from '../context/user';
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../context/user';
+import getUserByUserId from '../services/firebase';
 
 export default function usePhotos() {
   const [photos, setPhotos] = useState(null);
   const {
     user: { uid: userId = '' }
-  } = useContext(userContext);
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const { following } = await getUserByUserId(userId);
+      let followedUserPhotos = [];
+
+      // does the user actually follow people?
+      if (following.length > 0) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+    }
+
+    console.log(userId);
+  }, []);
 
   return { photos };
 }
