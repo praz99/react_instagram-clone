18ed8355f0491db3a71feb5fd594587392dbbdf4
Praz <t.prazwal@gmail.com>
2021-06-09 00:49:21 +0200

    Add the feature to like an image Add actions component inside the Post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Image from './Image';
+import Actions from './actions';
 
 function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
     </div>
   );
 }
