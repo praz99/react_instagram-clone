8c142df10d5e39c536a3209a00b97d9f419f92fa
Praz <t.prazwal@gmail.com>
2021-06-10 22:29:59 +0200

    Add a function getUserByUsername in services/firebase use that function in Profile component to get user

--- src/components/post/Comments.js
+++ src/components/post/Comments.js
@@ -11,9 +11,7 @@ function Comments({ docId, comments: allComments, posted, commentInput }) {
     <>
       <div className="p-4 pt-1 pb-4">
         {comments.length >= 3 && (
-          <p className="text-sm text-gray-base mb-1 cursor-pointer">
-            View all {comments.length} comments
-          </p>
+          <p className="text-sm text-gray-base mb-1 cursor-pointer">View all comments</p>
         )}
         {comments.slice(0, 3).map((item) => (
           <p key={`${item.comment}-${item.displayName}`} className="mb-1">
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { getUserByUsername } from '../services/firebase';
 
 function Profile() {
-  return null;
+  const { username } = useParams();
+  const [userExists, setUserExists] = useState(false);
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const doesUserExist = await getUserByUsername(username);
+    }
+  }, []);
+
+  return <p>Hello</p>;
 }
 
 Profile.propTypes = {};
