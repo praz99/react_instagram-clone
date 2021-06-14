ed91e0eb6db0e7ed5ad461efe3beac04e8907ab8
Praz <t.prazwal@gmail.com>
2021-06-14 18:09:23 +0200

    Display the profile picture and username of a user in profile page

--- src/components/profile/Header.js
+++ src/components/profile/Header.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import useUser from '../../hooks/use-user';
+import { isUserFollowingProfile } from '../../services/firebase';
 
-function Header() {
-  return null;
+function Header({
+  photosCount,
+  followerCount,
+  setFolloowerCount,
+  profile: { docId: profileDocId, userId: profileUserId, fullName, following = [] }
+}) {
+  const { user } = useUser();
+  const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+
+  useEffect(() => {
+    const isLoggedInUserFollowingProfile = async () => {
+      const isFollowing = await isUserFollowingProfile(user.username, profileUserId);
+      setIsFollowingProfile(isFollowing);
+    };
+
+    if (user.username && profileUserId) {
+      isLoggedInUserFollowingProfile();
+    }
+  }, [user.username, profileUserId]);
+  return (
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      <div className="container flex justify-center">
+        {user.username && (
+          <img
+            className="rounded-full h-40 w-40 flex"
+            alt={`${user.username} profile`}
+            src={`/images/avatars/${user.username}.jpg`}
+          />
+        )}
+      </div>
+      <div className="flex items-center justify-center flex-col col-span-2">
+        <div className="container flex items-center">
+          <p className="text-2xl mr-4">{user.username}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-Header.propTypes = {};
+Header.propTypes = {
+  photosCount: PropTypes.number.isRequired,
+  followerCount: PropTypes.number.isRequired,
+  setFolloowerCount: PropTypes.func.isRequired,
+  profile: PropTypes.shape({
+    docId: PropTypes.string,
+    userId: PropTypes.string,
+    fullName: PropTypes.string,
+    following: PropTypes.array
+  }).isRequired
+};
 
 export default Header;
