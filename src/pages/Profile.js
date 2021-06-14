80756fec8ebf95bdbfe866f19cfb467e7b406e97
Praz <t.prazwal@gmail.com>
2021-06-14 15:43:58 +0200

    get user's fullName and display

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
 
 function Profile() {
   const { username } = useParams();
+  const [user, setUser] = useState(null);
   const [userExists, setUserExists] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
     async function checkUserExists() {
-      const doesUserExist = await getUserByUsername(username);
+      const user = await getUserByUsername(username);
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExists(true);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
     }
-  }, []);
 
-  return <p>Hello</p>;
+    checkUserExists();
+  }, [username, history]);
+
+  return userExists ? (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">{user.fullName}</div>
+    </div>
+  ) : null;
 }
 
 Profile.propTypes = {};
